10bcdeae5fc8f5bad43357aa097f53add1478f44
fashraf186 <fashraf@xavor.com>
2018-11-20 13:43:38 +0500

    Sms

---
+++ Authenic_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Authenic_Project.Startup))]
+namespace Authenic_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
